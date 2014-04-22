16a86426aef4e8a1ab87d5e3f4e7659459f252ef
Jan Ulrich <jan@optemo.com>
2014-04-21 20:13:12 -0700

    Added website

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Optemocom::Application.config.secret_token = 'f56cf8bbf2e000ed3b549a469dcde67c003b647d52fa1c0eb164c0aee37d6cf5aa1303f8e42c50f49257bde0beea0d2fb96bc3d1d51f0d8335ae884d506e1a87'
--- test/unit/helpers/optemo_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class OptemoHelperTest < ActionView::TestCase
-end
